d2a3415c6a5a5291330cbc3cc94d99cdca40f876
olegmicode <olegmijovic.io@gmail.com>
2024-06-25 14:35:23 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ const nodemailer = require("nodemailer");
 const BrevoTransport = require("nodemailer-brevo-transport");
 require("dotenv").config();
 
-
-
-
 const APP_PORT = process.env.APP_PORT || 3000;
-const APP_HOST = process.env.APP_HOST || '0.0.0.0';
+const APP_HOST = process.env.APP_HOST || "0.0.0.0";
 const BREVO_API_KEY = process.env.BREVO_API_KEY;
 const SMTP_EMAIL_ADDRESS = process.env.SMTP_EMAIL_ADDRESS || 3000;
-const SMTP_EMAIL_USERNAME = process.env.SMTP_EMAIL_USERNAME || '0.0.0.0';
+const SMTP_EMAIL_USERNAME = process.env.SMTP_EMAIL_USERNAME || "0.0.0.0";
 
 const app = express();
 app.use(bodyParser.json());
@@ -23,8 +20,16 @@ const transporter = nodemailer.createTransport(
 );
 
 app.post("/send-email-by-brevo", async (req, res) => {
-  const { recipient, subject, htmlContent } =
-    req.body;
+  const { recipient, subject, htmlContent } = req.body;
+  if (!SMTP_EMAIL_ADDRESS || !SMTP_EMAIL_USERNAME || !transporter) {
+    res.status(400).send("env seems not configured");
+    return;
+  }
+  
+  if (!recipient || !subject || !htmlContent) {
+    res.status(400).send("parameter is not configured");
+    return;
+  }
 
   const mailOptions = {
     from: {
